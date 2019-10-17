0007582ad98a457da96475cea311e0d422748909
EdisonBedoya98 <edison.bedoyag@udea.edu.co>
2019-10-17 12:55:37 -0500

    pedidos update

--- src/verPedido/Pedido.js
+++ src/verPedido/Pedido.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import CarouserlThumbnaile from './CarouserlThumbnail';
 import '../verPedido/Pedido.scss';
 
 class Pedido extends Component {
     render() {
         return (
-               <tr>
+               <tr key={this.props.key}>
                 <td>{this.props.name}</td>
                 <td>{this.props.payment_method}</td>
                 <td>{this.props.shipment_address}</td>
